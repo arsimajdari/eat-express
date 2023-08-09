ce5ba12c456983f81deda584da391f5b8473fb68
Arsim01 <arsim.ajdari@hoyo.tech>
2023-08-09 13:47:04 +0200

    authentication updated

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,4 @@ Route::resource('products', ProductController::class)->middleware('auth:sanctum'
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
